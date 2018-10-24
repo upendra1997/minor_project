0d00cd84e12a46f0c969a818632762f908cc6f8f
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2018-10-24 05:38:57 +0000

    added requirements.txt

--- server.py
+++ server.py
@@ -1,13 +1,31 @@
 from flask import Flask
 from analyzer import analyze
+import os
 
-app = Flask(__name__)
+host = 'localhost'
+if "IP" in os.environ.keys():
+    host = os.environ["IP"]
+
+port = 8080
+if "PORT" in os.environ.keys():
+    port = os.environ["PORT"]
+
+
+
+app = Flask(__name__,static_url_path = "/front-end")
+
 object = analyze()
 
-@app.route('/<text>')
+@app.route('/',methods=["GET"])
+def front_end():
+    print("hit")
+    print(app.send_static_file('/front-end/front-end.html'))
+    return app.send_static_file('/front-end/front-end.html')
+
+@app.route('/<text>',methods=["POST"])
 def sentiment_analyze(text):
     result = object.input(text)
     print(text,result)
     return str(result)
 
-app.run()
+app.run(host=host,port=port,debug=True)
